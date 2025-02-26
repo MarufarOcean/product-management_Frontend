dc7f351f31020bf8ca80b9474d0a7ee82642b90e
MarufarOcean <marufarocean@gmail.com>
2025-02-26 08:15:59 +0600

    updated product.services.ts

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ProductService {
-  private apiUrl = 'http://localhost:5000/api/products';
+  private apiUrl = 'https://localhost:7187/api/Products';
 
   constructor(private http: HttpClient) { }
 
